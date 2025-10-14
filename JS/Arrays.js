338d56e8d3bf9619826334dc14bafce10d0521cb
dadodopa <dopazodomingues@gmail.com>
2025-10-14 10:56:23 +0200

    14/10/2025

--- JS/Arrays.js
+++ JS/Arrays.js
@@ -1,3 +1,4 @@
+//* Función para mostrar 
 export function mostrarNumeroElementos(array) {
     return array.length();
 }
@@ -7,14 +8,36 @@ export function mostrarElementos(array, id) {
     });
 }
 export function mostrarElementosReves(array, id) {
-    array.reverse();
+    [...array].reverse();
     array.forEach(element => {
         document.getElementById(id).innerHTML = `${element}`
     });
 }
 export function mostrarElementosAlfabeticamente(array, id) {
-    array.sort();
+//TODO Crea una copia del array y se trabaja sobre la copia no modific el original
+    [...array].sort();
     array.forEach(element => {
         document.getElementById(id).innerHTML = `${element}`
     });
+}
+export function anadirPrincipio(array, element) {
+    array.unshift(element);
+}
+
+export function anadirFinal(array, element) {
+    array.push(element);
+}
+
+export function borrarPrincipio(array) {
+    let aeliminar= array[0];
+    array.shift();
+    return aeliminar;
+}
+export function borrarFinal(array) {
+    let aeliminar=array.length-1;
+    array.pop();
+    return aeliminar;
+}
+export function mostrarElemento(array,numero) {
+    return array[numero];
 }
